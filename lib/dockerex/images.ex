1f24586828a77d59d34d96ffa198c10aad8029bc
Ángel Herranz <aherranz@fi.upm.es>
2021-03-15 10:45:22 +0100

    Documentation of module Dockerex.Images + refactor to use Dockerex.process_httpoison_resp in get, create, build and remove

--- lib/dockerex/images.ex
+++ lib/dockerex/images.ex
@@ -1,7 +1,13 @@
 defmodule Dockerex.Images do
+  @moduledoc """
+  Module that interfaces the image related API of the Docker Engine
+  API. See https://docs.docker.com/engine/api/ for more information.
+  """
+
   use Dockerex.Images.Types
   require Logger
 
+  # TODO(AH): adapt spec and implementation to Dockerex.process_httpoison_resp
   @spec list(ListParams.t()) ::
           {:ok, [ImageAbstract.t()]} | {:error, :bad_request, map()} | {:error, :request_error}
   def list(options \\ nil) do
@@ -28,51 +34,37 @@ defmodule Dockerex.Images do
     end
   end
 
-  @spec get(String.t()) :: {:ok, Image.t()} | {:error, :not_found | :request_error}
+  @doc """
+  Return low-level information about an image.
+  """
+  @spec get(String.t()) :: {:ok, Image.t()} | Dockerex.engine_err()
   def get(id) do
-    case HTTPoison.get(Dockerex.get_url("/images/#{id}/json")) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Poison.decode!(body, keys: :atoms)}
+    opts = Dockerex.add_options()
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, :not_found}
-
-      resp ->
-        Logger.error("#{inspect(resp)}")
-        {:error, :request_error}
-    end
+    HTTPoison.get(Dockerex.get_url("/images/#{id}/json"), opts)
+    |> Dockerex.process_httpoison_resp()
   end
 
-  @spec create(CreateParams.t(), binary() | nil) ::
-          {:ok, String.t()}
-          | {:error, :bad_request | :not_found, map()}
-          | {:error, :request_error}
+  @doc """
+  Create an image by either pulling it from a registry or importing it.
+  """
+  @spec create(CreateParams.t(), binary() | nil) :: {:ok, [map()]} | Dockerex.engine_err()
   def create(params, image \\ nil) do
     url = Dockerex.get_url("/images/create", params)
     headers = Dockerex.add_auth()
+    opts = Dockerex.add_options()
 
-    case HTTPoison.post(url, image || "", headers, []) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, body}
-
-      {:ok, %HTTPoison.Response{status_code: 400, body: body}} ->
-        {:error, :bad_request, Poison.decode!(body)}
-
-      {:ok, %HTTPoison.Response{status_code: 404, body: body}} ->
-        {:error, :not_found, Poison.decode!(body)}
-
-      resp ->
-        Logger.error("#{inspect(resp)}")
-        {:error, :request_error}
-    end
+    HTTPoison.post(url, image || "", headers, opts)
+    |> Dockerex.process_httpoison_resp(progress: true)
   end
 
+  @doc """
+  Build an image from a tar archive with a Dockerfile in it.
+  """
   @spec build(BuildParams.t(), binary() | nil, map()) ::
-          {:ok, String.t() | nil, String.t()}
-          | {:error, :bad_request, map()}
-          | {:error, :request_error}
-          | {:error, :build_error, BuildError.t()}
-
+          {:ok, String.t(), [map()]}
+          | {:error, :build_error, String.t() | map()}
+          | Dockerex.engine_err()
   def build(params, image \\ nil, registry_config \\ %{}) do
     params =
       case Map.get(params, :cachefrom, nil) do
@@ -105,20 +97,20 @@ defmodule Dockerex.Images do
     headers = Dockerex.add_registry_config(registry_config)
     options = Dockerex.add_options()
 
-    case HTTPoison.post(url, image || "", headers, options) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        progress = Dockerex.decode_progress(body)
-
+    HTTPoison.post(url, image || "", headers, options)
+    |> Dockerex.process_httpoison_resp(progress: true)
+    |> case do
+      {:ok, progress} ->
         errors =
           for progress_line <- progress,
               Map.has_key?(progress_line, :errorDetail) or Map.has_key?(progress_line, :error) do
             progress_line
           end
 
+        error = "Cannot extract image ID from #{inspect(progress)}"
+
         case errors do
           [] ->
-            error = "Cannot extract image ID from digest in #{inspect(body)}"
-
             auxs =
               for progress_line <- progress,
                   Map.has_key?(progress_line, :aux) do
@@ -127,7 +119,7 @@ defmodule Dockerex.Images do
 
             case auxs do
               [%{ID: id} | _] ->
-                {:ok, id, body}
+                {:ok, id, progress}
 
               _ ->
                 Logger.error(error)
@@ -135,18 +127,16 @@ defmodule Dockerex.Images do
             end
 
           [error1 | _errors] ->
+            Logger.error(error)
             {:error, :build_error, error1}
         end
 
-      {:ok, %HTTPoison.Response{status_code: 400, body: body}} ->
-        {:error, :bad_request, Poison.decode!(body, keys: :atoms)}
-
-      resp ->
-        Logger.error("#{inspect(resp)}")
-        {:error, :request_error}
+      response ->
+        response
     end
   end
 
+  # TODO(AH): adapt spec and implementation to Dockerex.process_httpoison_resp
   @spec prune(map() | nil) :: {:ok, map()} | {:error, :request_error}
   def prune(params \\ nil) do
     params =
@@ -178,25 +168,15 @@ defmodule Dockerex.Images do
     end
   end
 
-  @spec remove(String.t(), RemoveParams.t()) ::
-          {:ok, RemoveResponse.t()} | {:error, :conflict | :not_found | :request_error}
+  @doc """
+  Remove an image, along with any untagged parent images that were referenced by that image.
+  """
+  @spec remove(String.t(), RemoveParams.t()) :: {:ok, RemoveResponse.t()} | Dockerex.engine_err()
   def remove(id, params \\ nil) do
     url = Dockerex.get_url("/images/#{id}", params)
     options = Dockerex.add_options()
 
-    case HTTPoison.delete(url, %{}, options) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Poison.decode!(body, keys: :atoms)}
-
-      {:ok, %HTTPoison.Response{status_code: 409}} ->
-        {:error, :conflict}
-
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, :not_found}
-
-      resp ->
-        Logger.error("#{inspect(resp)}")
-        {:error, :request_error}
-    end
+    HTTPoison.delete(url, %{}, options)
+    |> Dockerex.process_httpoison_resp()
   end
 end
